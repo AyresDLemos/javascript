fc46cd9d8c1f3d1da7bfd12b645b4fa75651b9ad
ayres <ayresdlemos@outlook.com>
2025-04-24 15:27:40 -0300

    Boolean e Condicionais

---
+++ script-aula.js
@@ -0,0 +1,26 @@
+var PossuiGraduacao = true;
+
+if (PossuiGraduacao) {
+  console.log("É verdadeiro!");
+} else {
+  console.log("È falso!");
+}
+
+var PossuiGraduacao = false;
+var PossuiDoutorado = true;
+
+if (PossuiGraduacao) {
+  console.log("È verdadeiro!");
+} else if (PossuiDoutorado) {
+  console.log("PossuiDoutorado");
+} else {
+  console.log("Não possui nada!");
+}
+
+var nome = "Ayres";
+
+if (nome) {
+  console.log(nome);
+} else {
+  console.log("Nome não exisate");
+}
